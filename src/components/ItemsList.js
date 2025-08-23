f46f5d0c2e10646ba70b676ef6b9abef1388925e
rounakranjan5 <rounakranjanjha@gmail.com>
2025-08-23 20:44:35 +0530

    Res-Category Accordion Added

---
+++ src/components/ItemsList.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const ItemsList = (props) => {
+    console.log(props);
+  return (
+    <div>
+
+        <div>
+            <ul>
+                {
+                    props.props.map((item)=>{
+                        return <li key={item?.card?.info?.id} className='flex justify-between flex-col  m-3 p-3 flex-wrap gap-2 rounded-lg border-gray-300 shadow-xl bg-gray-100 cursor-pointer '>
+                            <div className="flex-row  flex-wrap ">
+                                <h4 className='text-red-950 font-semibold text-lg mb-2'>{item?.card?.info?.name}
+                                </h4>
+
+
+                                <div className='mb-1 flex justify-center gap-2 flex-wrap-reverse md:flex-nowrap md:justify-between'>
+                                  
+                                      <h4 className='text-wrap'>{item?.card?.info?.description}</h4>
+                                      <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/"+item?.card?.info?.imageId} className='rounded-md '/>
+                                   
+                                </div>
+
+                                <h4 className='font-semibold text-yellow-700 text-2xl '>₹{item?.card?.info?.price/100 || item?.card?.info?.finalPrice/100 || item?.card?.info?.variantsV2?.pricingModels[0]?.price/100}</h4>
+
+
+                          </div>
+
+                            </li>
+                    })
+                }
+            </ul>
+        </div>
+
+    </div>
+  )
+}
+
+export default ItemsList
--- src/components/ResCategory.js
+++ src/components/ResCategory.js
@@ -1,11 +1,33 @@
-const ResCategory=({data})=>{
+import { useState } from "react";
+import ItemsList from "./ItemsList";
+
+const ResCategory = ({ data }) => {
     console.log("Category data:", data);
+    let [isAccordionOpen,setisAccordionOpen] = useState(false);
+    function handleAccordion(){
+        setisAccordionOpen(!isAccordionOpen);
+    }
     return (
         <div>
-            <div className="w-11.5/12 flex justify-between flex-wrap  border-gray-200 bg-gray-100 m-3 p-3  rounded-lg shadow-md hover:bg-gray-200 cursor-pointer text-lg">
-                <span className="font-semibold">{data?.card?.card?.title} ({data?.card?.card?.itemCards?.length}) </span>
-                <span>⬇️</span>
+
+            <div className="w-11.5/12   border-gray-200 bg-gray-100 m-3 p-3  rounded-lg shadow-md hover:bg-gray-200 cursor-pointer text-lg cursor-pointer mb-5" onClick={handleAccordion}>
+
+                <div className="flex justify-between flex-wrap">
+                    <span className="font-semibold">{data?.card?.card?.title} ({data?.card?.card?.itemCards?.length}) </span>
+                    <span>⬇️</span>
+                </div>
+
+                
+                {isAccordionOpen && <div>
+                    <ItemsList props={data?.card?.card?.itemCards} />
+                </div>}
+
             </div>
+
+
+
+
+
         </div>
     )
 }
